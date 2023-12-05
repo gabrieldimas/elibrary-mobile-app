486ddb84ca1312e6f9583ecb7b688b6b0172ddad
Gabriel Dimas <93063866+gabrieldimas@users.noreply.github.com>
2023-12-05 23:32:47 +0700

    minor update

--- simbesi/lib/helloworld.dart
+++ simbesi/lib/helloworld.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(const Center(
-    child: Text(
-      'Hello World!',
-      textDirection: TextDirection.ltr,
-    ),
+    child: Text('Hello World', textDirection: TextDirection.ltr),
   ));
 }
