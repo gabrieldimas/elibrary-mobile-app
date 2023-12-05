68adae61c20548dad82cbb262bbd4ac34b0cc6c3
Gabriel Dimas <93063866+gabrieldimas@users.noreply.github.com>
2023-12-05 22:31:18 +0700

    minor update

--- simbesi/lib/main.dart
+++ simbesi/lib/main.dart
@@ -7,7 +7,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -31,7 +30,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Hello World'),
     );
   }
 }
@@ -106,7 +105,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Ditekan sebanyak:',
             ),
             Text(
               '$_counter',
